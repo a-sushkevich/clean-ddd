7baf445b05db16cbf026a604f39032c90d502f51
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2026-03-11 00:04:03 +0300

    product schema update

--- src/product/infrastructure/adapters/drizzle-product.repository.ts
+++ src/product/infrastructure/adapters/drizzle-product.repository.ts
@@ -1,9 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ProductRepository } from 'src/product/application/ports/product.repository.port';
-import {
-  DRIZZLE,
-  DrizzleDB,
-} from 'src/shared/infrastructure/database/postgres/drizzle.provider';
+import { DRIZZLE } from 'src/shared/infrastructure/database/postgres/drizzle.provider';
+import type { DrizzleDB } from 'src/shared/infrastructure/database/postgres/drizzle.provider';
 
 @Injectable()
 export class DrizzleProductRepository implements ProductRepository {
