67157b4adca842da1ea2b6518ee2dfcd46968ee6
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2026-03-10 23:58:29 +0300

    product schema

---
+++ src/product/infrastructure/adapters/drizzle-product.repository.ts
@@ -0,0 +1,11 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ProductRepository } from 'src/product/application/ports/product.repository.port';
+import {
+  DRIZZLE,
+  DrizzleDB,
+} from 'src/shared/infrastructure/database/postgres/drizzle.provider';
+
+@Injectable()
+export class DrizzleProductRepository implements ProductRepository {
+  constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
+}
---
+++ src/shared/infrastructure/database/postgres/schemas/products.schema.ts
@@ -0,0 +1,25 @@
+import { timestamp } from 'drizzle-orm/pg-core';
+import {
+  uuid,
+  varchar,
+  pgTable,
+  integer,
+  text,
+  boolean,
+} from 'drizzle-orm/pg-core';
+
+export const products = pgTable('products', {
+  id: uuid('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  description: text('description').notNull(),
+  sku: varchar('sku', { length: 100 }).notNull().unique(),
+  priceAmount: integer('price_amount').notNull(),
+  priceCurrency: varchar('price_currency', { length: 3 })
+    .notNull()
+    .default('USD'),
+  stock: integer('stock').notNull().default(0),
+  isActive: boolean('is_active').notNull().default(true),
+  lowStockThreshold: integer('low_stock_treshold').notNull().default(5),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
