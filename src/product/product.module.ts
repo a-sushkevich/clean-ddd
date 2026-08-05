9fa57099c1cf2b05fc8e6b2d004522770276d800
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2026-08-05 17:03:01 +0200

    dynamic database

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ProductsController } from './presentation/product.controller';
 import { PRODUCT_REPOSITORY } from './application/ports/product.repository.port';
-import { DrizzleProductRepository } from './infrastructure/adapters/drizzle-product.repository';
 import { CommandHandlers } from './application';
 import { QueryHandlers } from './application/queries/handlers';
+import { ConfigService } from '@nestjs/config';
+import { MongoProductRepository } from './infrastructure/adapters/mongo-product.repository';
+import { DrizzleProductRepository } from './infrastructure/adapters/drizzle-product.repository';
 
 @Module({
   imports: [CqrsModule],
@@ -12,9 +14,20 @@ import { QueryHandlers } from './application/queries/handlers';
   providers: [
     ...CommandHandlers,
     ...QueryHandlers,
+    MongoProductRepository,
+    DrizzleProductRepository,
     {
       provide: PRODUCT_REPOSITORY,
-      useClass: DrizzleProductRepository,
+      useFactory: (
+        configService: ConfigService,
+        mongoRepo: MongoProductRepository,
+        drizzleRepo: DrizzleProductRepository,
+      ) => {
+        return configService.get('DATABASE') === 'mongodb'
+          ? mongoRepo
+          : drizzleRepo;
+      },
+      inject: [ConfigService, MongoProductRepository, DrizzleProductRepository],
     },
   ],
 })
