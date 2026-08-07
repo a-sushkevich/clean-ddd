c7eadc699b1614edd685a3d4851087c16c6d59f2
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2026-08-07 15:22:09 +0200

    customer registered event and handler

--- src/customers/application/use-cases/register-customer/register-customer.handler.ts
+++ src/customers/application/use-cases/register-customer/register-customer.handler.ts
@@ -1,4 +1,4 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { RegisterCustomerCommand } from './register-customer.command';
 import { Inject } from '@nestjs/common';
 import {
@@ -20,6 +20,7 @@ export class RegisterCustomerHandler implements ICommandHandler<
   constructor(
     @Inject(CUSTOMER_REPOSITORY)
     private readonly customerRepository: CustomerRepositoryPort,
+    private readonly eventPublisher: EventPublisher,
   ) {}
   async execute(command: RegisterCustomerCommand): Promise<void> {
     const email = Email.create(command.email);
@@ -33,13 +34,17 @@ export class RegisterCustomerHandler implements ICommandHandler<
       );
     }
 
-    const customer = Customer.register(
-      email,
-      command.firstName,
-      command.lastName,
-      command.phone,
+    const customer = this.eventPublisher.mergeObjectContext(
+      Customer.register(
+        email,
+        command.firstName,
+        command.lastName,
+        command.phone,
+      ),
     );
 
     await this.customerRepository.save(customer);
+
+    customer.commit();
   }
 }
